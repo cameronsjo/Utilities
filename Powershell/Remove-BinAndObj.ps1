9358f5a6aa4acc0fe4fa59765a8d0b837d9dd3f4
Cameron Sjo <csjo00@gmail.com>
2016-11-14 11:23:54 -0600

    Update Remove-BinAndObj.ps1

--- Powershell/Remove-BinAndObj.ps1
+++ Powershell/Remove-BinAndObj.ps1
@@ -1,4 +1,4 @@
 # Recursively deletes bin and obj files from the current directory
 # This is great for cleaning and fresh-starting from a directory
 # http://stackoverflow.com/a/5924807/2607840
-Get-ChildItem .\ -Include bin,obj -Recurse | foreach ($_) { Remove-Item $_.fullname -Force -Recurse }
+Get-ChildItem .\ -Include bin,obj -Recurse | foreach ($_) { Remove-Item $_.FullName -Force -Recurse }
