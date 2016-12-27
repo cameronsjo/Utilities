e615a2270957546a2e5f683d28a7010f53c65e7a
Cameron Sjo <csjo00@gmail.com>
2016-12-27 08:18:52 -0600

    Create Get-ProjectReferences.ps1

---
+++ Powershell/Get-ProjectReferences.ps1
@@ -0,0 +1,20 @@
+# http://stackoverflow.com/a/10472358/2607840
+Function Get-ProjectReferences ($rootFolder)
+{
+    $projectFiles = Get-ChildItem $rootFolder -Filter *.csproj -Recurse
+    $ns = @{ defaultNamespace = "http://schemas.microsoft.com/developer/msbuild/2003" }
+
+    $projectFiles | ForEach-Object {
+        $projectFile = $_ | Select-Object -ExpandProperty FullName
+        $projectName = $_ | Select-Object -ExpandProperty BaseName
+        $projectXml = [xml](Get-Content $projectFile)
+
+        $projectReferences = $projectXml | Select-Xml '//defaultNamespace:ProjectReference/defaultNamespace:Name' -Namespace $ns | Select-Object -ExpandProperty Node | Select-Object -ExpandProperty "#text"
+
+        $projectReferences | ForEach-Object {
+            "[" + $projectName + "] -> [" + $_ + "]"
+        }
+    }
+}
+
+Get-ProjectReferences $pwd | Out-File references.txt
