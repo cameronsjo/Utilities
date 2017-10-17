9e668c10f553afb590a28efaf929c6f53a6a4647
Cameron Sjo <csjo00@gmail.com>
2017-10-17 15:08:37 -0500

    Update Remove-Suo.ps1

--- Powershell/Remove-Suo.ps1
+++ Powershell/Remove-Suo.ps1
@@ -1,1 +1,8 @@
-Get-ChildItem -Force -Recurse -Include *.suo | ForEach ($_) { Remove-Item $_.FullName - Force }
+# Recursively deletes all the suo files in the parent folder.
+# Suo files are a binary file that's used by Visual Studio to contain preferences.
+# Usually when Visual Studio gets janky or is jacked up the first step is to remove all the suo files.
+#
+# As always, you'll need to make sure that Visual Studio is closed before running this otherwise
+# it'll probably have a lock on the file and make powershell blow out.
+
+Get-ChildItem -Force -Recurse -Include *.suo | ForEach ($_) { Remove-Item $_.FullName -Force }
