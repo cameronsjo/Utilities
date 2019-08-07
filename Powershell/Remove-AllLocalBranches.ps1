67c8975fa79f455d50e3ca227fd6bc0f0c91242d
Cameron Sjo <csjo00@gmail.com>
2019-08-07 09:21:03 -0500

    Update Remove-AllLocalBranches.ps1

--- Powershell/Remove-AllLocalBranches.ps1
+++ Powershell/Remove-AllLocalBranches.ps1
@@ -1,3 +1,13 @@
 # Could use lowercase "d" in delete for a "safe delete"
 # https://stackoverflow.com/a/57314194/2607840
-git branch --format '%(refname:lstrip=2)' | Where-Object { $_ -ne 'master' } | % { git branch -D $_ }
+
+# Remove local branches
+git branch --format '%(refname:lstrip=2)' `
+  | Where-Object { $_ -ne 'master' } `
+  | % { git branch -D $_ }
+
+
+# Remove remote branches
+git branch --format  '%(refname:lstrip=2)' -r `
+  | Where-Object { $_.StartsWith('origin') -And !($_.Contains('master')) } `
+  | % { git push origin --delete $_.Substring(7) }
